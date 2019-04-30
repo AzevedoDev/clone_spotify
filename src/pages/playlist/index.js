7bdd10dcca1d2255b6f7ff336ad5e091da1d7c51
Pedro Azevedo <theazevedo@outlook.com>
2019-04-30 01:14:15 -0300

    add playlistDetails, add proptypes and fix bug

--- src/pages/playlist/index.js
+++ src/pages/playlist/index.js
@@ -1,137 +1,126 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as PlaylistDetailsActions } from '../../store/ducks/playlistDetails';
 import { Container, Header, SongList } from './styles';
+
 import ClockIcon from '../../assets/images/clock.svg';
 import PlusIcon from '../../assets/images/plus.svg';
+import Loading from '../../components/Loading';
 
-const Playlist = () => (
-  <Container>
-    <Header>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/pt/thumb/8/8b/Sandy-Junior-2001.jpg/220px-Sandy-Junior-2001.jpg"
-        alt="Playlist"
-      />
-      <div>
-        <span>Playlist</span>
-        <h1>Rock Forever</h1>
-        <p>13 músicas</p>
+class Playlist extends Component {
+  static propTypes = {
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.number,
+      }),
+    }).isRequired,
 
-        <button>PLAY</button>
-      </div>
-    </Header>
-    <SongList cellPadding={0} cellSpacing={0}>
-      <thead>
-        <th />
-        <th>Título</th>
-        <th>Artista</th>
-        <th>Album</th>
-        <th>
-          <img src={ClockIcon} alt="Duracação" />
-        </th>
-      </thead>
+    getPlaylistDetailsRequest: PropTypes.func.isRequired,
+    playlistDetails: PropTypes.shape({
+      data: PropTypes.arrayOf(
+        PropTypes.shape({
+          thumbnail: PropTypes.string,
+          title: PropTypes.string,
+          description: PropTypes.string,
+          songs: PropTypes.arrayOf(
+            PropTypes.shape({
+              id: PropTypes.number,
+              title: PropTypes.title,
+              author: PropTypes.string,
+              album: PropTypes.string,
+            }),
+          ),
+        }),
+      ),
+      loading: PropTypes.bool,
+    }).isRequired,
+  };
 
-      <tbody>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-        <tr>
-          <td>
-            <img src={PlusIcon} alt="Adicionar" />
-          </td>
-          <td>4 Estações</td>
-          <td>Sandy & Junior</td>
-          <td>4 Estações</td>
-          <td>3:26</td>
-        </tr>
-      </tbody>
-    </SongList>
-  </Container>
-);
+  componentDidMount() {
+    this.loadPlaylistDetails();
+  }
 
-export default Playlist;
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.id !== this.props.match.params.id) {
+      this.loadPlaylistDetails();
+    }
+  }
+
+  loadPlaylistDetails = () => {
+    const { id } = this.props.match.params;
+    this.props.getPlaylistDetailsRequest(id);
+  };
+
+  renderDetails = () => {
+    const playlist = this.props.playlistDetails.data;
+
+    return (
+      <Container>
+        <Header>
+          <img src={playlist.thumbnail} alt={playlist.title} />
+          <div>
+            <span>Playlist</span>
+            <h1>{playlist.title}</h1>
+            {!!playlist.songs && <p>{playlist.songs.length} música</p>}
+
+            <button>PLAY</button>
+          </div>
+        </Header>
+        <SongList cellPadding={0} cellSpacing={0}>
+          <thead>
+            <th />
+            <th>Título</th>
+            <th>Artista</th>
+            <th>Album</th>
+            <th>
+              <img src={ClockIcon} alt="Duracação" />
+            </th>
+          </thead>
+
+          <tbody>
+            {!playlist.songs ? (
+              <tr>
+                <td colSpan={5}>Nenhuma música cadastrada</td>
+              </tr>
+            ) : (
+              playlist.songs.map(song => (
+                <tr key={song.id}>
+                  <td>
+                    <img src={PlusIcon} alt="Adicionar" />
+                  </td>
+                  <td>{song.title}</td>
+                  <td>{song.author}</td>
+                  <td>{song.album}</td>
+                  <td>3:26</td>
+                </tr>
+              ))
+            )}
+          </tbody>
+        </SongList>
+      </Container>
+    );
+  };
+
+  render() {
+    return this.props.playlistDetails.loading ? (
+      <Container loading>
+        <Loading />
+      </Container>
+    ) : (
+      this.renderDetails()
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  playlistDetails: state.playlistDetails,
+});
+const mapDispatchToProps = dispatch => bindActionCreators(PlaylistDetailsActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Playlist);
